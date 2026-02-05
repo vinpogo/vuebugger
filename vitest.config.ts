fdfe7e7588a6554e75aa4ba27c6440b137ab57aa
vinpogo <git@grossmayer.vin>
2026-02-05 21:21:19 +0100

    conf(ci): add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: { include: ['src/**/*.test.ts'] },
+  test: {
+    include: ['src/**/*.test.ts'],
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['dot', 'github-actions']
+      : ['dot'],
+  },
 })
