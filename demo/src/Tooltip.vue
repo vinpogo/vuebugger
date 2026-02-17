ecc6500749fe97c0ec0ce2d79407ff50e5ad45cf
vinpogo <git@grossmayer.vin>
2026-02-17 15:08:33 +0100

    feat(demo): comprehensive demo for both packages

---
+++ demo/src/Tooltip.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useTooltip } from '@vingy/vueltip'
+import { useTemplateRef } from 'vue'
+
+const tooltipElement = useTemplateRef('tooltipElement')
+const arrowElement = useTemplateRef('arrowElement')
+
+const { tooltipStyles, arrowStyles, show, content } =
+  useTooltip({
+    tooltipElement,
+    arrowElement,
+    offset: 8,
+    padding: 8,
+  })
+</script>
+
+<template>
+  <div
+    v-show="show"
+    ref="tooltipElement"
+    class="relative isolate"
+    :style="[tooltipStyles]"
+  >
+    <div
+      ref="arrowElement"
+      class="pointer-events-none absolute z-0 bg-slate-100"
+      :style="[arrowStyles]"
+    />
+    <div
+      class="relative z-10 rounded bg-slate-100 px-2 py-1 text-sm font-semibold text-slate-900 shadow-lg"
+    >
+      {{ content?.text }}
+    </div>
+  </div>
+</template>
--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,10 +1,15 @@
 import Vuebugger from '@vingy/vuebugger'
+import { tooltipPlugin } from '@vingy/vueltip'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Tooltip from './Tooltip.vue'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(Vuebugger)
+app
+  .use(createPinia())
+  .use(Vuebugger)
+  .use(tooltipPlugin, { component: Tooltip })
 
 app.mount('#app')
