aa6e3f97f11c291289b3c97653c13916365261c6
vinpogo <git@grossmayer.vin>
2026-02-18 10:13:20 +0100

    feat(vueltip): proper typescript support

--- packages/vueltip/src/plugin.ts
+++ packages/vueltip/src/plugin.ts
@@ -1,21 +1,21 @@
 import { App, Component, createApp } from 'vue'
-import { tooltipDirective } from './directive'
 import { setOptions } from './options'
 import type { Options } from './types'
 
-export const tooltipPlugin = {
+export const vueltipPlugin = {
   install: (
     app: App,
-    options: Partial<Options> & { component: Component },
+    options: Partial<Options & { component: Component }>,
   ) => {
-    setOptions(options)
-    app.directive('tooltip', tooltipDirective)
+    const { component, ...rest } = options
+    setOptions(rest)
+    if (!component) return
 
     const container = document.createElement('div')
     container.id = '__vueltip_root__'
     document.body.appendChild(container)
 
-    const tooltipApp = createApp(options.component)
+    const tooltipApp = createApp(component)
     tooltipApp._context = app._context // Share the plugin context
     tooltipApp.mount(container)
   },
