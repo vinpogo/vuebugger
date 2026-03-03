c69f1a49fb732c9218b48c80227716846cfbbfd4
vinpogo <git@grossmayer.vin>
2026-03-03 14:55:40 +0100

    ref(vuebugger): properly type ImportMeta

--- packages/vuebugger/src/env.d.ts
+++ packages/vuebugger/src/env.d.ts
@@ -1,9 +1,7 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
-  readonly DEV: boolean
+  readonly DEV?: boolean
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env?: ImportMetaEnv
 }
