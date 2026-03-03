171f570611937979ee897fc9c3eeeae3902feecf
vinpogo <git@grossmayer.vin>
2026-03-03 14:56:01 +0100

    ref(vuebugger): extract plugin into file

--- packages/vuebugger/src/index.ts
+++ packages/vuebugger/src/index.ts
@@ -1,17 +1,6 @@
-import type { App, Plugin } from 'vue'
-import { setUidGenerator } from './debug'
-import { setupComposableDevtools } from './devtools'
-import { PluginOptions } from './types'
+import { plugin } from './plugin'
 
 export { debug } from './debug'
 
-const plugin: Plugin<[PluginOptions?]> = {
-  install: (app: App, options?: PluginOptions) => {
-    if (!import.meta.env?.DEV) return
-    if (options?.uidFn) setUidGenerator(options.uidFn)
-    setupComposableDevtools(app)
-  },
-}
-
-export { plugin as DebugPlugin }
+export { plugin as DebugPlugin, plugin as Vuebugger }
 export default plugin
